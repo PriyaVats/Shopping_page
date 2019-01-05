f398778c407f7d1d15b9a76fe6c45eeba69772de
Priya Vats <priyavats1438@gmail.com>
2019-01-06 00:59:17 +0530

    viewOfPurchasedDetails

---
+++ customer1.js
@@ -0,0 +1,53 @@
+var user=[];
+user=getStoredUser();
+var order=[];
+order=getStoredOrderCart();
+display();
+function display()
+{
+	for(var i=1;i<user.length;i++)
+	{
+		var userid=user[i].userid;
+		    appendInList(userid,document.getElementById("p1"));
+			appendInList(user[i].username,document.getElementById("p2"));
+		//alert(order[userid].length+"userid is : "+userid);
+		var product=" ";
+		var cost=0;
+		for(var j=0;j<order[userid].length;j++)
+		{
+			product=product+order[userid][j].name+" ";
+			cost=cost+order[userid][j].price;
+			//alert(order[userid][j].roll+"  "+order[userid][j].name);
+		}
+		
+		appendInList(product,document.getElementById("p3"));
+		appendInList(cost,document.getElementById("p4"));
+	}
+}
+function appendInList(a,b)
+{
+	var li=document.createElement("li");
+	li.textContent=a;
+	b.appendChild(li);
+	var br=document.createElement("br");
+    b.appendChild(br);
+	   	
+}
+function getStoredUser() 
+{
+	if (!localStorage.user)
+	{ 
+    localStorage.user = JSON.stringify([]); 
+    } 
+    return JSON.parse(localStorage.user); 
+
+}
+function getStoredOrderCart()
+ {
+	if (!localStorage.ordercart)
+    { 
+	// default to empty array 
+	localStorage.ordercart = JSON.stringify([]);
+	} 
+	return JSON.parse(localStorage.ordercart); 
+}
